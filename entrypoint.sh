711acc63e590302ff1d636b8ef164bc1faa8a4a1
hanzy1110 <hanzy1110@github.com>
2023-03-16 15:05:48 -0300

    new vies

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 echo "Entrypoint..."
 # source .env.prod
 echo "Setting up database..."
-sh -c "python manage.py migrate"
+# sh -c "python manage.py migrate"
 sh -c "python manage.py createsuperuser --no-input"
 
 sleep 10
