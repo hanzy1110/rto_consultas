3c838db7b7a858130d7653f37858c8d36468033f
hanzy1110 <hanzy1110@github.com>
2023-03-16 10:23:48 -0300

    build

--- build_AWS.sh
+++ build_AWS.sh
@@ -3,22 +3,22 @@
 echo "Working dir"
 echo $(pwd)
 
-docker-compose --env-file .env down --remove-orphans
-docker-compose --env-file .env build
+sudo docker-compose --env-file .env down --remove-orphans
+sudo docker-compose --env-file .env build
 
 echo MAKING AND APPLYING MIGRATIONS...
-docker-compose --env-file .env run --rm rto_consultas -c "python manage.py makemigrations && python manage.py migrate"
-# docker-compose --env-file .env run --rm crm_api sh -c "python manage.py migrate"
+sudo docker-compose --env-file .env run --rm rto_consultas python manage.py makemigrations && python manage.py migrate
+# sudo docker-compose --env-file .env run --rm crm_api sh -c "python manage.py migrate"
 
-docker-compose --env-file .env up -d 
+sudo docker-compose --env-file .env up -d 
 
 echo Waiting for containers...
 sleep 10
-docker ps -a 
-docker logs -t rto_mysql_db
+sudo docker ps -a 
+sudo docker logs -t rto_mysql_db
 
 echo "----------------------<>-----------------------"
-docker logs -t --follow rto_consultas
+sudo docker logs -t --follow rto_consultas
 
-# docker exec -it crm_api bash 
+# sudo docker exec -it crm_api bash 
 
