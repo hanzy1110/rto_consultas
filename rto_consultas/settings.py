f4a21acd78f960a56a84f9aacb300e81e8399cea
hanzy1110 <hanzy1110@github.com>
2023-03-20 10:32:36 -0300

    commited

--- rto_consultas/settings.py
+++ rto_consultas/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'rto_consultas'
+    'rto_consultas'
 ]
 
 MIDDLEWARE = [
