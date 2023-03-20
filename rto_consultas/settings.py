b5b007f71411244e1fa54f6c6ab6f5d6f513ee0b
hanzy1110 <hanzy1110@github.com>
2023-03-20 10:45:53 -0300

    admin

--- rto_consultas/settings.py
+++ rto_consultas/settings.py
@@ -49,8 +49,8 @@ INSTALLED_APPS = [
     # 'django.contrib.auth',
     # 'django.contrib.contenttypes',
     # 'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.messages',
+    # 'django.contrib.staticfiles',
     'rto_consultas'
 ]
 
