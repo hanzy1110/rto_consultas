937e69c7550b480221d772b3fc0aa2b9455bfde0
hanzy1110 <hanzy1110@github.com>
2023-03-20 10:44:32 -0300

    gogo

--- rto_consultas/settings.py
+++ rto_consultas/settings.py
@@ -45,10 +45,10 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    # 'django.contrib.admin',
+    # 'django.contrib.auth',
+    # 'django.contrib.contenttypes',
+    # 'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rto_consultas'
