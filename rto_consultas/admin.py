dc035e1d9fa6fed915a60bfe7c1bd0926e5979d7
hanzy1110 <hanzy1110@github.com>
2023-03-20 11:36:22 -0300

    adminsite

---
+++ rto_consultas/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+import rto_consultas.models as models
+
+admin.site.register(models.Verificaciones)
+admin.site.register(models.Certificadosasignadosportaller)
+admin.site.register(models.Vehiculos)
