e709b9481d4d20de8ecd241d52297458d8ffa259
hanzy1110 <hanzy1110@github.com>
2023-03-21 11:33:11 -0300

    prefetch2

--- rto_consultas/views.py
+++ rto_consultas/views.py
@@ -3,8 +3,12 @@ from .models import Verificaciones, Certificadosasignadosportaller
 
 class ListVerificacionesView(ListView):
 	model = Verificaciones
+	paginate_by = 10
+
+	def get_queryset(self):
+		return self.model.objects.all().prefectch_related()[:5]
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
-		context['Certificados'] = self.model.objects.all().prefectch_related()[:5]
+		# context['Certificados'] = 
 		return context
