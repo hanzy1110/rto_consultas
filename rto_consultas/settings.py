5d4cea4bbb008eb00f69b5fd3411c2948dba1a10
hanzy1110 <hanzy1110@github.com>
2023-03-21 10:14:42 -0300

    pathin2

--- rto_consultas/settings.py
+++ rto_consultas/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'rto_consultas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [APP_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
