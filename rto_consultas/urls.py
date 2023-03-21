0d672dac29ffb6ba56632b7215e6c5e9cd6e6c51
hanzy1110 <hanzy1110@github.com>
2023-03-21 13:44:40 -0300

    templateBoogaloo

--- rto_consultas/urls.py
+++ rto_consultas/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import ListVerificacionesView
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("/", include("django.contrib.auth.urls")),  
-    path('verificaciones/', ListVerificacionesView.as_view())
+    path("accounts/", include("django.contrib.auth.urls")),  
+    path('verificaciones/', ListVerificacionesView.as_view()),
+    path('', TemplateView.as_view(template_name='home.html'), name='home')
 ]
