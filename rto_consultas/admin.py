c9ee80efe550cac9522bad77854f440e664f6918
hanzy1110 <hanzy1110@github.com>
2023-03-21 13:20:57 -0300

    more

--- rto_consultas/admin.py
+++ rto_consultas/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 import rto_consultas.models as models
 
 class VerificacionesFilter(admin.ModelAdmin):
-    list_display = ('idtaller', 'idtipouso', 'idestado')
+    # list_display = ('idtaller', 'idtipouso', 'idestado')
     # list_filter = ('idtaller', 'dominiovehiculo', 'idtipouso', 'idestado' )
     search_fields = ('dominiovehiculo',)
 
