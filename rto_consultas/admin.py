63e1a6e3575e31be122788a09a4d80568342a738
hanzy1110 <hanzy1110@github.com>
2023-03-21 13:16:13 -0300

    tuples

--- rto_consultas/admin.py
+++ rto_consultas/admin.py
@@ -4,7 +4,7 @@ import rto_consultas.models as models
 class VerificacionesFilter(admin.ModelAdmin):
     list_display = ('idtaller', 'idtipouso', 'idestado')
     # list_filter = ('idtaller', 'dominiovehiculo', 'idtipouso', 'idestado' )
-    search_fields = ('dominiovehiculo')
+    search_fields = ('dominiovehiculo',)
 
 admin.site.register(models.Verificaciones, VerificacionesFilter)
 admin.site.register(models.Certificados)
