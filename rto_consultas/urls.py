b305e8370e0dd14071bb49cc2899ce2a46ec1687
hanzy1110 <hanzy1110@github.com>
2023-03-22 10:59:23 -0300

    enter the dashboard

--- rto_consultas/urls.py
+++ rto_consultas/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),  
     path('verificaciones/', ListVerificacionesView.as_view()),
+    path('dashboard/', include('dashboard.urls')), 
     path('', TemplateView.as_view(template_name='home.html'), name='home')
 ]
