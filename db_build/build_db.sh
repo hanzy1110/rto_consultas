3c0394d281beaf58aa7f1d66dc80557dc0dc0ccd
hanzy1110 <hanzy1110@github.com>
2023-04-03 15:55:44 -0300

    built

--- db_build/build_db.sh
+++ db_build/build_db.sh
@@ -15,9 +15,4 @@ echo "----------------------<>-----------------------"
 echo Waiting for containers...
 sleep 10
 sudo docker ps -a 
-sudo docker logs -t rto_mysql_db
-
-echo "----------------------<>-----------------------"
-sudo docker logs -t --follow rto_consultas
-
-# sudo docker exec -it crm_api bash 
+sudo docker logs -tf rto_mysql_db
