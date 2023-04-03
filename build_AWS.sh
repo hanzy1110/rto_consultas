631b50d8b95675eb106d9f87553d893b4f4e4ef6
hanzy1110 <hanzy1110@github.com>
2023-04-03 11:26:47 -0300

    built

--- build_AWS.sh
+++ build_AWS.sh
@@ -4,7 +4,7 @@ echo "Working dir"
 echo $(pwd)
 
 sudo docker-compose --env-file envfiles/.env down --remove-orphans 
-sudo docker-compose --env-file envfiles/.env build --progress plain --no-cache
+sudo docker-compose --env-file envfiles/.env build --progress plain
 
 echo MAKING AND APPLYING MIGRATIONS...
 # sudo docker-compose --env-file envfiles/.env run --rm rto_consultas bash -c "python manage.py makemigrations && python manage.py migrate --fake-initial"
