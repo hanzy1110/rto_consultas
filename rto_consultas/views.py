f3ea3e9169ee6427660c03c9758eef5b77015c49
hanzy1110 <hanzy1110@github.com>
2023-05-02 15:28:13 -0300

    all

--- rto_consultas/views.py
+++ rto_consultas/views.py
@@ -15,8 +15,8 @@ def handle_args(query_params, queryset):
     for key, arg in cleaned_query.items():
         if numeric_test.match(str(arg)):
             query = Q(**{f"{key}__exact":int(arg)})
-        # elif "dominio" in key: 
-        #     query = Q(**{f"{key}__icontains":arg}) 
+        elif "dominio" in key: 
+            query = Q(**{f"{key}__exact":arg}) 
         elif isinstance(arg, str):
             query = Q(**{f"{key}__icontains":arg})
         else:
