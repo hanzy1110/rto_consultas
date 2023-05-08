786a33bbc9ffd13a82ea93d351074e0bce45ad18
hanzy1110 <hanzy1110@github.com>
2023-05-08 15:14:32 -0300

    wrongonew

--- rto_consultas/templatetags/getattr.py
+++ rto_consultas/templatetags/getattr.py
@@ -39,7 +39,7 @@ def query_dict(context, instance, name):
 @register.simple_tag(takes_context=True)
 def query_descriptions(context, descriptions, field, value):
     try:
-        return descriptions[1][field][value]
+        return descriptions[field][value[1]]
     except KeyError as e:
         print(e)
         return "Desconocido"
