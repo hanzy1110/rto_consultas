24aed977f7de6751483de6a9e366db4b7f970d93
hanzy1110 <hanzy1110@github.com>
2023-06-29 10:53:39 -0300

    migrations

--- build_AWS.sh
+++ build_AWS.sh
@@ -6,8 +6,8 @@ sudo docker-compose --env-file envfiles/.env down --remove-orphans
 sudo docker-compose --env-file envfiles/.env build --progress plain
 
 echo MAKING AND APPLYING MIGRATIONS...
-# sudo docker-compose --env-file envfiles/.env run --rm rto_consultas bash -c "python manage.py makemigrations && python manage.py migrate --fake-initial"
-# sudo docker-compose --env-file .env run --rm crm_api sh -c "python manage.py migrate"
+sudo docker-compose --env-file envfiles/.env run --rm rto_consultas bash -c "python manage.py makemigrations && python manage.py migrate --fake-initial"
+sudo docker-compose --env-file .env run --rm crm_api sh -c "python manage.py migrate"
 
 sudo docker-compose --env-file envfiles/.env up -d
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,11 @@
 echo "Entrypoint..."
 # source .env.prod
 echo "Setting up database..."
-python manage.py makemigrations
-python manage.py migrate
+# python manage.py makemigrations
+python manage.py migrate --fake-initial rto_consultas
 python manage.py createsuperuser --no-input
 
 sleep 10
 echo "Starting Server..."
-
-#  python manage.py collectstatic
-# Start the Django development server with watchdog
-# watchmedo auto-restart --directory=./ --pattern=*.py --recursive -- \
-#     python manage.py runserver 0.0.0.0:8000
 
 python manage.py runserver 0.0.0.0:${WEB_PORT}
