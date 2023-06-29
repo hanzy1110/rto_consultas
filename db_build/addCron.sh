73fddc6a1a0df54e5081ca96c6fad0b855574ec6
hanzy1110 <hanzy1110@github.com>
2023-06-29 14:44:15 -0300

    yes

--- db_build/addCron.sh
+++
@@ -1,5 +0,0 @@
-sudo cp rcloneS3 /etc/cron.d/rcloneS3
-# Give execution rights on the cron job
-chmod 0644 /etc/cron.d/rcloneS3
-curl https://rclone.org/install.sh | bash 
-chmod +x /home/ubuntu/rto_central_deploy/deploy/central_mysql_db/rclone.sh
