72dad696f603a828d3e733ad44dbb4229301a23a
hanzy1110 <hanzy1110@github.com>
2023-07-10 16:27:06 -0300

    a

--- rto_consultas/urls.py
+++ rto_consultas/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import rto_consultas.views as views
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
