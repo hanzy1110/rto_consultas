948a1367537b7933f99b4a32d06d328d48a9d45c
hanzy1110 <hanzy1110@github.com>
2023-07-13 15:21:23 -0300

    gogo

--- rto_consultas/views.py
+++ rto_consultas/views.py
@@ -216,14 +216,14 @@ class VerVerificacion(DetailView,LoginRequiredMixin):
                                .get(idcategoria__exact=cert[0]["idcategoria"])
                                .descripcion)
 
-        categoria = (Estados.objects
+        estado = (Estados.objects
                                .get(idestado__exact=cert[0]["idestado"])
                                .descripcion)
 
         context["nrocertificado"] = cert[0]["nrocertificado"]
         context["observaciones"] = cert[0]["observaciones"]
         context["vigenciahasta"] = cert[0]["vigenciahasta"]
-        context["estado"] = cert[0]["estado"]
+        context["estado"] = estado
         context["categoria"] = categoria
         # TODO AGREGAR COMBO DOC
         context["comboDoc"] = []
