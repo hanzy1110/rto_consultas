7704275a788ec9aa567bee8e98863480fdb94ad8
bm69 <bm69@pop-os.localdomain>
2023-07-21 10:31:01 -0300

    all

--- data_analysis/analysis.py
+++ data_analysis/analysis.py
@@ -4,17 +4,29 @@ import pandas as pd
 import numpy as np
 import pymc as pm
 import arviz as az
+import matplotlib.pyplot as plt
+import pytensor.tensor as pt
 
 YEAR = 2015
 DATA = pathlib.Path(os.getcwd()) / f"dataset/joined/{YEAR}_CERTIFICADOS.csv"
 
-data = pd.read_csv(DATA).loc["Aprobado", "RechazadoLeveModerado", "RechazadoGrave"]
+data = pd.read_csv(DATA)
+data = data[["Aprobado", "RechazadoLeveModerado", "RechazadoGrave"]]
 
 print(data.head())
 with pm.Model() as coin_flipping:
-    p = pm.Uniform('p', lower=0, upper=1, shape=(3,))
-    y = pm.Bernoulli('y', p=p, observed=data.head(100))
+    p = pm.Dirichlet('p', np.ones(3))
+    y = pm.Bernoulli('y', p=p, observed=data)
     trace = pm.sample(1000, tune=1000)
-    # reset value to get the shape right
-    ppc = pm.sample_posterior_predictive(trace)
-    az.traceplot(trace)
+    # ppc = pm.sample_posterior_predictive(trace)
+    az.plot_trace(trace)
+    plt.savefig(f"analysis_data/traceplot_{YEAR}.png")
+    print(az.summary(trace))
+    trace.to_netcdf(f"analysis_data/trace_{YEAR}.nc")
+
+fig, ax = plt.subplots(2,1, figsize=(15,15))
+az.plot_posterior(trace, ax=ax[0])
+# print(ppc.posterior_predictive)
+# az.plot_ppc(ppc, ax=ax[1])
+# ax[1].plot(ppc)
+plt.savefig(f"analysis_data/ppc_{YEAR}.png")
