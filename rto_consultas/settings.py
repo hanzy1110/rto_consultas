2a4ea65c70c03f1fc66469dc593e3d9e98741d04
hanzy1110 <hanzy1110@github.com>
2023-08-07 12:44:51 -0300

    requirements

--- rto_consultas/settings.py
+++ rto_consultas/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'silk.middleware.SilkyMiddleware',
+    # 'silk.middleware.SilkyMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
