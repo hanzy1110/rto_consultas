73554f317d30df0eb19582c127f912a2dea8f55d
hanzy1110 <hanzy1110@github.com>
2023-08-25 15:31:51 -0300

    sll

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 echo "Entrypoint..."
 # source .env.prod
 echo "Setting up database..."
-# python manage.py makemigrations --merge --noinput
+python manage.py makemigrations --merge --noinput
 python manage.py migrate --fake-initial rto_consultas
 python manage.py collectstatic --noinput
 python manage.py createsuperuser --no-input
