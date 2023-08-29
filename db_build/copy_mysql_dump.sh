66841c368bf8d23cfaf35245805b8f725a0bc29d
hanzy1110 <hanzy1110@github.com>
2023-08-29 12:04:42 -0300

    secrets

--- db_build/copy_mysql_dump.sh
+++ db_build/copy_mysql_dump.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+set -xe
+
+source envfiles/.env
 
 # Remote server details
 REMOTE_SERVER="azureSVR"
 REMOTE_DUMP_PATH="/tmp/dump.sql"
-MYSQL_USER="replication_user"
-MYSQL_PASSWORD="repl_pass2023"
 
 # Local directory for dump file
 LOCAL_DUMP_DIR="/home/ubuntu/central_dump"
 # LOCAL_DUMP_FILE="dump.sql"
 
 # MySQL dump command
-MYSQLDUMP_CMD="mysqldump -u ${MYSQL_USER} -p ${MYSQL_PASSWORD} > $REMOTE_DUMP_PATH"
+MYSQLDUMP_CMD="mysqldump -u ${MYSQL_DUMP_USER} -p ${MYSQL_DUMP_PASSWORD} > $REMOTE_DUMP_PATH"
 
 # Create a database dump on the remote server
 ssh $REMOTE_SERVER "${MYSQLDUMP_CMD}"
