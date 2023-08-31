725af741b4ede90e2173fd6601a9a3ae7ee4f9cc
hanzy1110 <hanzy1110@github.com>
2023-08-31 10:20:59 -0300

    replication status

--- db_build/db_setup.sh
+++ db_build/db_setup.sh
@@ -1,12 +1,14 @@
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER exporter@rto_mysql_exporter IDENTIFIED BY ${MYSQL_PASSWORD} GRANT ALL PRIVILEGES ON *.* TO exporter@rto_mysql_exporter WITH GRANT OPTION"
+set -xe
+
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER exporter@rto_mysql_db IDENTIFIED BY ${MYSQL_PASSWORD} GRANT ALL PRIVILEGES ON *.* TO exporter@rto_mysql_db WITH GRANT OPTION"
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES"
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE ${MYSQL_DATABASE}"
+# mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE ${MYSQL_DATABASE}"
 
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CHANGE MASTER TO \
 MASTER_HOST=40.87.111.221,
 MASTER_USER='replication_user',
 MASTER_PASSWORD='repl_pass2023',
-MASTER-LOG-FILE=$FILL_WITH_MASTER_STATUS_FILE
-MASTER-LOG-POS=$FILL_WITH_MASTER_STATUS_COORDS
+MASTER-LOG-FILE=mysql-bin.000009
+MASTER-LOG-POS=181968
 "
 # mysql -u root centralnqn < /app/ScriptSQL/centralNqn_210504.sql
