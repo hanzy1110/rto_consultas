ca85f1da3d1508ea02e42dcfe5e0337406280513
hanzy1110 <hanzy1110@github.com>
2023-09-29 14:17:43 -0300

    options

--- rto_consultas/forms.py
+++ rto_consultas/forms.py
@@ -21,8 +21,8 @@ class ObleasPorTaller(forms.Form):
     fecha_desde = forms.DateField(required=False, label="fecha_desde")
     fecha_hasta = forms.DateField(required=False, label="fecha_hasta")
     taller_id = forms.ChoiceField(
-        # choices=[("option1", "Option 1"), ("option2", "Option 2")],
-        choices=list(tuple(map_fields(aux_data, Talleres)["idtaller"])),
+        choices=[("option1", "Option 1"), ("option2", "Option 2")],
+        # choices=list(tuple(map_fields(aux_data, Talleres)["idtaller"])),
         required=True,
         label="Planta",
     )
