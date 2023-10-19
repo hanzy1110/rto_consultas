8a868d02fc507105cab8f925d5df412109dfdc16
hanzy1110 <hanzy1110@github.com>
2023-10-19 14:56:04 -0300

    settings again

--- rto_consultas/settings.py
+++ rto_consultas/settings.py
@@ -161,7 +161,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = "staticfiles/"
+STATIC_URL = "staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
