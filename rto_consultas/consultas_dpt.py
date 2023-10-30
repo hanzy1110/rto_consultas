4cab6833dbcca9d7b015c3001c6a860cdcae0910
hanzy1110 <hanzy1110@github.com>
2023-10-30 18:45:41 -0300

    lo que estan mal no soy yo

--- rto_consultas/consultas_dpt.py
+++ rto_consultas/consultas_dpt.py
@@ -1,4 +1,5 @@
 import os
+from typing import Union
 import requests
 from dataclasses import dataclass, asdict
 import json
@@ -37,16 +38,34 @@ class DPTResponse:
     dict = asdict
 
 
+@dataclass
+class HabsResponse:
+    Hoy: str
+    InicHabilit: str
+    NumHabilit: str
+    VehiAnio: str
+    VehiCapac: str
+    VehiInt: str
+    VehiPat: str
+    VtoHabilit: str
+    VtoRevTec: str
+    VtoSeguro: str
+
+    dict = asdict
+
+
 API_MODES = {"1": "Vehiculos", "2": "Habilitaciones"}
 
 
-def query_dpt(form_data: dict) -> DPTResponse:
+def query_dpt(form_data: dict) -> Union[DPTResponse, HabsResponse]:
     logger.info(f"DOMINIO => {form_data}")
     client = requests.Session()
 
+    consulta = API_MODES[form_data["consulta"]]
+
     endpoint = "http://10.0.0.17:60000/dpt_request"
     client.headers.update({"Content-Type": "application/json"})
-    data = {"dominio": form_data["dominio"], "mode": API_MODES[form_data["consulta"]]}
+    data = {"dominio": form_data["dominio"], "mode": consulta}
 
     response = client.get(endpoint, data=data)
     logger.debug(f"DPT RESPONSE => {response}")
@@ -56,5 +75,11 @@ def query_dpt(form_data: dict) -> DPTResponse:
     # dpt_response = response.json()
     logger.debug(f"INFO => {dpt_response}")
 
-    return DPTResponse(**dpt_response)
-    # return dpt_response
+    match consulta:
+        case "Vehiculos":
+            return DPTResponse(**dpt_response)
+        case "Habilitaciones":
+            return HabsResponse(**dpt_response)
+        # just to please the linter
+        case _:
+            return HabsResponse(**dpt_response)
