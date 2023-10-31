3102597b67f66fa5a20a991fb9650f9e14672bac
hanzy1110 <hanzy1110@github.com>
2023-10-31 14:39:03 -0300

    el casco blanco

--- rto_consultas/routers.py
+++ rto_consultas/routers.py
@@ -1,11 +1,12 @@
+DJANGO_TABLES = ["auth", "admin", "contenttypes"]
 class UserRouter:
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'auth' or model._meta.app_label == 'your_user_app':
+        if model._meta.app_label in DJANGO_TABLES:
             return 'users'
         return 'default'
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label == 'auth' or model._meta.app_label == 'your_user_app':
+        if model._meta.app_label in DJANGO_TABLES:
             return 'users'
         return 'default'
 
@@ -13,6 +14,6 @@ class UserRouter:
         return True
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if app_label == 'auth' or app_label == 'your_user_app':
+        if app_label in DJANGO_TABLES:
             return db == 'users'
         return db == 'default'
