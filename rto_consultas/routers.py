9a54aace46989530a9d2b256e4f9192352a746f8
hanzy1110 <hanzy1110@github.com>
2023-10-31 14:45:52 -0300

    todo bien

--- rto_consultas/routers.py
+++ rto_consultas/routers.py
@@ -1,4 +1,4 @@
-DJANGO_TABLES = ["auth", "admin", "contenttypes"]
+DJANGO_TABLES = ["auth", "admin", "contenttypes", "sessions"]
 class UserRouter:
     def db_for_read(self, model, **hints):
         if model._meta.app_label in DJANGO_TABLES:
