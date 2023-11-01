45fc723685e3583e12b619d4d6f259ba003d6dc5
hanzy1110 <hanzy1110@github.com>
2023-11-01 14:29:36 -0300

    managed

--- rto_consultas_rn/urls.py
+++ rto_consultas_rn/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-import rto_consultas.views as views
+
+# TODO Check this kind of errors
+import rto_consultas_rn.views as views
 
 urlpatterns = [
-    path("/verificaciones_rn", views.ListVerificacionesView.as_view(), name="verificaciones_rn"),
+    path("verificaciones_rn", views.ListVerificacionesView.as_view(), name="verificaciones_rn"),
     path(
         "ververificacion/<int:idverificacion>/<int:idtaller>",
         views.VerVerificacion.as_view(),
