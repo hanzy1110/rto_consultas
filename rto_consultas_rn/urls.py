58193dff7afcd77f47986cfc12633ae3445359fd
hanzy1110 <hanzy1110@github.com>
2023-11-01 14:23:26 -0300

    missing n

--- rto_consultas_rn/urls.py
+++ rto_consultas_rn/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 import rto_consultas.views as views
 
-urlpatters = [
+urlpatterns = [
     path("/verificaciones_rn", views.ListVerificacionesView.as_view(), name="verificaciones_rn"),
     path(
         "ververificacion/<int:idverificacion>/<int:idtaller>",
