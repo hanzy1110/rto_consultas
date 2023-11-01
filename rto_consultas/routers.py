61e37424cf229d0c4dbe50efbd5acc6d7983dbe2
hanzy1110 <hanzy1110@github.com>
2023-11-01 15:13:53 -0300

    en un nivel

--- rto_consultas/routers.py
+++ rto_consultas/routers.py
@@ -1,17 +1,28 @@
+import os
+from .logging import configure_logger
+
+LOG_FILE      = os.environ["LOG_FILE"]
 DJANGO_TABLES = ["auth", "admin", "contenttypes", "sessions"]
+logger        = configure_logger(LOG_FILE)
 
 class UserRouter:
     def db_for_read(self, model, **hints):
+        logger.debug(f"READING FOR MODEL => {model}")
+        logger.debug(f"APP_LABEL => {model._meta.app_label}")
         if model._meta.app_label in DJANGO_TABLES:
             return 'users'
         elif model._meta.app_label == "rto_consultas_rn":
+            logger.debug(f"ROUTED TO => rio_negro")
             return "rio_negro"
         return 'default'
 
     def db_for_write(self, model, **hints):
+        logger.debug(f"READING FOR MODEL => {model}")
+        logger.debug(f"APP_LABEL => {model._meta.app_label}")
         if model._meta.app_label in DJANGO_TABLES:
             return 'users'
         elif model._meta.app_label == "rto_consultas_rn":
+            logger.debug(f"ROUTED TO => rio_negro")
             return "rio_negro"
         return 'default'
 
