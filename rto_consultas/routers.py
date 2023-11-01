92e52bbfcc39be7c3ecc039e3c929df667070d26
hanzy1110 <hanzy1110@github.com>
2023-11-01 16:13:21 -0300

    current stack logger gone

--- rto_consultas/routers.py
+++ rto_consultas/routers.py
@@ -21,7 +21,7 @@ class UserRouter:
     def db_for_write(self, model, **hints):
         logger.debug(f"READING FOR MODEL => {model}")
         logger.debug(f"APP_LABEL => {model._meta.app_label}")
-        logger.debug(f"CURRENT_STACK => {print_stack()}")
+        # logger.debug(f"CURRENT_STACK => {print_stack()}")
 
         if model._meta.app_label in DJANGO_TABLES:
             return 'users'
