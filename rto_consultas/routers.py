bb58842789c79e257fa6fa2961d1a798106e0fa3
hanzy1110 <hanzy1110@github.com>
2023-11-01 15:25:01 -0300

    make it crash

--- rto_consultas/routers.py
+++ rto_consultas/routers.py
@@ -9,6 +9,7 @@ class UserRouter:
     def db_for_read(self, model, **hints):
         logger.debug(f"READING FOR MODEL => {model}")
         logger.debug(f"APP_LABEL => {model._meta.app_label}")
+        assert False
         if model._meta.app_label in DJANGO_TABLES:
             return 'users'
         elif model._meta.app_label == "rto_consultas_rn":
@@ -19,6 +20,7 @@ class UserRouter:
     def db_for_write(self, model, **hints):
         logger.debug(f"READING FOR MODEL => {model}")
         logger.debug(f"APP_LABEL => {model._meta.app_label}")
+        assert False
         if model._meta.app_label in DJANGO_TABLES:
             return 'users'
         elif model._meta.app_label == "rto_consultas_rn":
