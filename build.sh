898538ef8adb51d7d2149bebfd0bd7130a86f4ad
hanzy1110 <hanzy1110@github.com>
2023-11-06 13:37:50 -0300

    build.sh changes!

--- build.sh
+++ build.sh
@@ -3,14 +3,15 @@ set -xe
 git pull
 
 function build_dev() {
-    sudo docker-compose -f docker-compose.dev.yml --env-file envfiles/.env down --remove-orphans
+    sudo docker-compose --env-file envfiles/.env rm -sv --force rto_consultas_dev
     sudo docker-compose -f docker-compose.dev.yml --env-file envfiles/.env build
     sudo docker-compose -f docker-compose.dev.yml --env-file envfiles/.env up -d
     return 0
 }
 
 function build_prod() {
-    sudo docker-compose --env-file envfiles/.env.prod down --remove-orphans
+    # sudo docker-compose --env-file envfiles/.env.prod down --remove-orphans
+    sudo docker-compose --env-file envfiles/.env.prod rm -sv --force rto_consultas
     sudo docker-compose --env-file envfiles/.env.prod build
     sudo docker-compose --env-file envfiles/.env.prod up -d
     return 0
@@ -19,6 +20,10 @@ function build_prod() {
 if [ "$1" == "dev" ]; then
     build_dev ""
 elif [ "$1" == "prod" ]; then
+    build_prod ""
+
+elif [ "$1" == "all" ]; then
+    build_dev ""
     build_prod ""
 fi
 
