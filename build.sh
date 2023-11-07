2a3f34f099bdc5ce32a500d1d5215048d92a3412
hanzy1110 <hanzy1110@github.com>
2023-11-07 09:27:55 -0300

    build_scriopt

--- build.sh
+++ build.sh
@@ -9,6 +9,13 @@ function build_dev() {
     return 0
 }
 
+function build_nginx() {
+    sudo docker-compose --env-file envfiles/.env.prod rm -sv --force rto_consultas_web_server
+    sudo docker-compose --env-file envfiles/.env.prod build
+    sudo docker-compose --env-file envfiles/.env.prod up -d
+    return 0
+}
+
 function build_prod() {
     # sudo docker-compose --env-file envfiles/.env.prod down --remove-orphans
     sudo docker-compose --env-file envfiles/.env.prod rm -sv --force rto_consultas_prod
@@ -26,6 +33,8 @@ elif [ "$1" == "prod" ]; then
 elif [ "$1" == "all" ]; then
     build_dev ""
     build_prod ""
+elif [ "$1" == "nginx" ]; then
+    build_nginx ""
 fi
 
 sudo docker ps -a
