ee0fb1e56ee1b3ad79d0eb663100ab1045bd8810
hanzy1110 <hanzy1110@github.com>
2023-11-08 11:04:09 -0300

    logo changes again

--- B_create_users.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'exporter'@'rto_mysql_db' IDENTIFIED BY '123';
-GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'rto_mysql_db';
-GRANT SELECT ON performance_schema.* TO 'exporter'@'rto_mysql_db';
-FLUSH PRIVILEGES;
-
