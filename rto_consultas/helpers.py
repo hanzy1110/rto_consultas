95822e27e64576a88b0e02e6b74ecde86996fe9b
hanzy1110 <hanzy1110@github.com>
2023-11-27 11:35:13 -0300

    push from file

--- rto_consultas/helpers.py
+++ rto_consultas/helpers.py
@@ -861,19 +861,19 @@ def handle_initial_cccf(nrocertificado, dominio):
 def handle_upload_file(files, s3_prefix, bucket_name=None):
     # TODO Upload to S3
     s3 = get_s3_client()
-    for k, f in files.items():
-        s3_key = f"{s3_prefix}/{k}"
-        with tempfile.TemporaryFile() as fp:
-            for chunk in f.chunks():
-                fp.write(chunk)
+    # for k, f in files.items():
+    s3_key = f"{s3_prefix}/{files.name}"
+    with tempfile.TemporaryFile() as fp:
+        for chunk in files.chunks():
+            fp.write(chunk)
 
-            # "s3_key is the path within the bucket"
-            upload_file_to_bucket(
-                file_path=fp.name,
-                bucket_name=bucket_name,
-                s3_client=s3,
-                s3_key=s3_key,
-            )
+        # "s3_key is the path within the bucket"
+        upload_file_to_bucket(
+            file_path=fp.name,
+            bucket_name=bucket_name,
+            s3_client=s3,
+            s3_key=s3_key,
+        )
 
 
 def allow_keys(data: dict, keys: list[str]):
