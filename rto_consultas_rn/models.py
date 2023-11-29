e9d1f40bc93271036e394f217985d86529fb223c
hanzy1110 <hanzy1110@github.com>
2023-11-29 15:30:38 -0300

    moar

--- rto_consultas_rn/models.py
+++ rto_consultas_rn/models.py
@@ -1657,7 +1657,7 @@ class Talleres(models.Model):
     )  # Field name made lowercase.
 
     def __str__(self):
-        return f"{self.nombre}".strip(" ")
+        return f"{self.idtaller}".replace(" ", "")
 
     class Meta:
         # managed = False
@@ -2616,8 +2616,8 @@ class Verificaciones(models.Model):
         return cert.nrocertificado
 
     def __str__(self) -> str:
-        return f"{self.idverificacion}_{self.dominiovehiculo}_{self.idtaller}".strip(
-            " "
+        return f"{self.idverificacion}_{self.dominiovehiculo}_{self.idtaller}".replace(
+            " ", ""
         )
 
     class Meta:
