6cea4f2b9991b96d3b9209e31086ff3aba87e0d8
hanzy1110 <hanzy1110@github.com>
2023-11-29 15:35:53 -0300

    stdio inheritance

--- entrypoint_prod.sh
+++ entrypoint_prod.sh
@@ -10,4 +10,4 @@ python manage.py createsuperuser --database users --no-input
 sleep 10
 echo "Starting Server..."
 # python manage.py runserver 0.0.0.0:${WEB_PORT}
-python -m gunicorn -b "0.0.0.0:${WEB_PORT}" --worker-class=gevent --worker-connections=1000 --workers=3 rto_consultas.wsgi:application
+python -m gunicorn -b "0.0.0.0:${WEB_PORT}" --enable-stdio-inheritance --worker-class=gevent --worker-connections=1000 --workers=3 rto_consultas.wsgi:application
