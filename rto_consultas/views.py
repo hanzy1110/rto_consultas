a92e398cd4626ac55d5730a68ab6dcf751a39749
hanzy1110 <hanzy1110@github.com>
2023-12-20 12:27:23 -0300

    capitales extranjeros

--- rto_consultas/views.py
+++ rto_consultas/views.py
@@ -296,14 +296,13 @@ class CustomRTOView(ExportMixin, SingleTableView, LoginRequiredMixin):
         context = super().get_context_data(**kwargs)
         context = handle_context(context, self)
 
+        context["item_count"] = self.object_list.count()
+
         logger.debug("CONTEXT HANDLED...")
         return context
 
     def get(self, request, *args, **kwargs):
         try:
-            logger.debug(f"KWARGS ==> {kwargs}")
-            logger.debug(f"ARGS ==> {args}")
-            assert False
             response = super().get(request, *args, **kwargs)
             return response
         except CertBoundError as e:
