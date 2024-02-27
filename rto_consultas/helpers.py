9c9bcd9baf695359849c2d06f66b9022d20352a6
hanzy1110 <hanzy1110@github.com>
2024-02-27 09:21:36 -0300

    done

--- rto_consultas/helpers.py
+++ rto_consultas/helpers.py
@@ -1504,7 +1504,7 @@ class TipoUsoAutocomplete(HTMXAutoComplete):
     # multiselect = True
     # minimum_search_length = 0
     _item_label = "descripcion"
-    _item_value = "idtipouso"
+    _item_value = "descripcion"
 
     def get_items(self, *args, **kwargs):
         logger.info(f"PARAMS LABEL => {self._item_label},VALUE => {self._item_value}, {self.route_name}")
