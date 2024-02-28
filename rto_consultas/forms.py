5ac5fb62b59243d1e4c76023e088abbb8c95a5e0
hanzy1110 <hanzy1110@github.com>
2024-02-28 09:55:38 -0300

    referencing

--- rto_consultas/forms.py
+++ rto_consultas/forms.py
@@ -289,7 +289,7 @@ class ConsultaDPTForm(forms.Form):
                     HTML("<b>Consulta Dominio/Habilitación Final"),
                     Field("dominio", wrapper_class="form-group col-4"),
                     Field("consulta", wrapper_class="form-group col-4"),
-                    # css_class="card card-plain mt-2 box",
+                    css_class="card card-plain mt-2 box",
                 ),
                     css_class="card card-plain mt-2 box",
             ),
