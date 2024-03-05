32772aa6e9be287006baebb495da4a730c4a549a
hanzy1110 <hanzy1110@github.com>
2024-03-05 15:20:27 -0300

    las que estan

--- rto_consultas/helpers.py
+++ rto_consultas/helpers.py
@@ -1296,7 +1296,7 @@ def get_resumen_data_mensual(cleaned_data, tipo_uso=None):
 
     v_reverif_totales = (
         v_reverificados.filter(idverificacionoriginal__in=v_anteriores)
-        # .exclude(handle_date_range(fecha_desde, fecha_hasta))
+        .filter(handle_date_range(fecha_desde, fecha_hasta))
         .values_list("idverificacion", "idtaller_id", "idverificacionoriginal")
     )
 
