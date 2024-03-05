8a0c0bfcc03c7fc7930711a1fe08f5a925efa079
hanzy1110 <hanzy1110@github.com>
2024-03-05 14:49:52 -0300

    probando

--- rto_consultas/helpers.py
+++ rto_consultas/helpers.py
@@ -1288,7 +1288,7 @@ def get_resumen_data_mensual(cleaned_data, tipo_uso=None):
     logger.info(f"V_REVERIFICADOS LEN {len(v_reverificados)}")
 
     v_anteriores = (Verificaciones.objects
-                    .filter(fecha__gt=fecha_desde)
+                    # .filter(fecha__gt=fecha_desde)
                     # .values_list("idverificacionoriginal", flat=True)
                     .values_list("idverificacion", flat=True)
                     )
