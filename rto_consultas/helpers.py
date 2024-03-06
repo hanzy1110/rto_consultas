425a05f3534bf4ef4a218157dd5a20918606e6b3
hanzy1110 <hanzy1110@github.com>
2024-03-06 11:14:44 -0300

    yes

--- rto_consultas/helpers.py
+++ rto_consultas/helpers.py
@@ -1341,7 +1341,7 @@ def get_resumen_data_mensual(cleaned_data, tipo_uso=None):
 
     logger.info(f"V_REVERIFICADOS_ANTERIORES LEN {len(v_reverificadas_anteriores)}")
     v_reverificado_a_cobrar = v_reverificados.difference(v_rev_anteriores)
-    verificaciones_a_cobrar = verificaciones_a_cobrar.intersection(v_reverificado_a_cobrar)
+    verificaciones_a_cobrar = verificaciones_a_cobrar.union(v_reverificado_a_cobrar)
     logger.info(f"VERIFICACIONES_A_COBRAR len => {len(verificaciones_a_cobrar)}")
 
     cobrados_queries = [
